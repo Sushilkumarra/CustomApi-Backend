068af0b3744bd2f192e927f1389f4770bcfafd9a
SushilRajput <invertissushil5171@gmail.com>
2025-11-07 08:54:39 +0530

    Added root route to fix Cannot GET / issue

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ mongoose.connect(process.env.MONGO_URI)
 // Routes
 app.use('/api/users', userRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
